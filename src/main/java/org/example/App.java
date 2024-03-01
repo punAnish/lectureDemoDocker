9fe8401ac33a5ce0233bda8a89322bcde986fe1f
AnishPun <ANISH.PUN@metropolia.fi>
2024-03-01 13:40:12 +0200

    second  commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,12 @@ package org.example;
  */
 public class App 
 {
+    public static int addMe (int a, int b) {
+        return a + b;
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println( "Hello World!" + addMe(1, 2));
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,21 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+    App app = new App();
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testAddMe() {
+        assertEquals(3, app.addMe(1, 2));
     }
 }
+
